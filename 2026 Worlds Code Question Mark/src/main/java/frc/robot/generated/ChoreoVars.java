1730dd91ae4bb234693c993993e61e1fd6cf85e3
KirbyC1 <cosmicplayz53@gmail.com>
2026-02-23 15:53:12 -0600

    Add files via upload

---
+++ 2026 Worlds Code Question Mark/src/main/java/frc/robot/generated/ChoreoVars.java
@@ -0,0 +1,16 @@
+package frc.robot.generated;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Units;
+import edu.wpi.first.units.measure.measure.*;
+
+/**
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
+ */
+public final class ChoreoVars {
+
+    private ChoreoVars() {}
+}
